5ac1ecc34a207cdb2b361baff1ae5b47f73e732d
aolguin-or <aolguin@dbsolution.cl>
2020-11-18 23:08:17 -0300

    Se elimina archivo ojdbc8.zip y directorio bin

--- bin/install-ojdbc8-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mvn install:install-file -Dfile=<ojdbc8_directory_path>/ojdbc8.jar -DgroupId=com.oracle -DartifactId=ojdbc8 -Dversion=18.3.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=<ojdbc8_directory_path>/ucp.jar -DgroupId=com.oracle.jdbc -DartifactId=ucp -Dversion=18.3.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=<ojdbc8_directory_path>/osdt_core.jar -DgroupId=com.oracle.jdbc -DartifactId=osdt_core -Dversion=18.3.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=<ojdbc8_directory_path>/osdt_cert.jar -DgroupId=com.oracle.jdbc -DartifactId=osdt_cert -Dversion=18.3.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=<ojdbc8_directory_path>/oraclepki.jar -DgroupId=com.oracle.jdbc -DartifactId=oraclepki -Dversion=18.3.0.0 -Dpackaging=jar
